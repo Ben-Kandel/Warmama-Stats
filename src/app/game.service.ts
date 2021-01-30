96682e2369f00d8c2a5263679103142a1e9a79d5
Ben-Kandel <deltaforce126@gmail.com>
2021-01-30 10:15:49 -0500

    fixed bug with disappearing match header

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 // import { Observable, throwError } from 'rxjs';
 // import { catchError, retry } from 'rxjs/operators';
 
-let DEBUG: boolean = false;
+let DEBUG: boolean = true;
 
 @Injectable({
   providedIn: 'root'
